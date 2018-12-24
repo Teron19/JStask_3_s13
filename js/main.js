3309e02cc801dcc9dec82db2597828216f2d9300
Teron19 <vladmordasov0@gmail.com>
2018-12-24 22:47:24 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,32 @@
+get
+
+
+
+
+
+
+function getRndNumber() {
+
+}
+
+let result = function setResult() {
+
+}
+
+function isNumbersEqual() {
+
+    result
+}
+
+functoin isBigDifference() {
+
+    result
+}
+
+function isTotal() {
+    //розпечатьть с помощью result
+}
+
+function run() {
+
+}
