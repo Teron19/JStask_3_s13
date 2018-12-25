ebfe2ecc4a4dfa20a8418e748a85ba09cd6b0ca9
Teron19 <vladmordasov0@gmail.com>
2018-12-25 22:43:48 +0200

    1

--- homeWork_13/js/task_2.js
+++ homeWork_13/js/task_2.js
@@ -1,3 +1,4 @@
+/*
 var btn = document.getElementById("play");
 var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
@@ -12,16 +13,16 @@ function getNameById() {
     switch (getPlayerResult) {
         let variant;
         case 1:
-        variant = "камень";
-            breack;
+            variant = "камень";
+            return;
         case 2:
-        variant = "ножницы";
-            breack;
+            variant = "ножницы";
+            return;
         case 3:
-        variant =  "бумага";
-            breack;
+            variant = "бумага";
+            return;
     }
-}
+} //не нужно return; оно в switch case
 
 function determineWinner(getPlayerResult) {
     (player1 === 1 && player2 === 2 || player1 === 2 && player2 === 3 || player1 === 1 && player2 === 3) ?
@@ -50,3 +51,4 @@ function runGame() {
 
 
 btn.addEventListener("click", getNameById);
+*/
--- js/task_1.js
+++ js/task_1.js
@@ -9,66 +9,71 @@
 
 4. Сделать функцию для определения совпадений. (isNumbersEqual). Она должна содержать в себе проверку на совпадение и внутри себя вызывать функцию для сохранения данных в общую строку (setResult).
 
-5. Сделать функцию для определения разницы. (isBigDifference). Она должна содержать в себе соответствующую проверку и внутри себя вызывать функцию для сохранения данных в общую строку (setResult).
+5. Сделать функцию для определения разницы. (isBigDifference). 
+Она должна содержать в себе соответствующую проверку и внутри себя вызывать функцию для сохранения данных в общую строку (setResult).
 
 6. Сделать функцию для вычисления результата total. Функция должна вычислять результат и сохранять его в переменную total.
 
 7. Сделать функцию, которая напечатает полученные с помощью функции setResult данные в HTML (printResult).*/
 
-'use strict'
+'use strict';
+
 let elemResult = document.getElementById('result');
+let writeResult = '';
+let total = 0,
+    first = 0,
+    second = 0;
+
+    //получения случайных чисел
+ let  getRndNumber = () =>  (Math.floor((Math.random() * 6) + 1));
 
 
-function getRndNumber() {  //получения случайных чисел
-    var first = Math.floor((Math.random() * 6) + 1);
-    var second = Math.floor((Math.random() * 6) + 1);
+
+const result = function setResult(str) { //склеивать все строки в одну
+    writeResult += str;
+    // writeResult += "Первая кость: " + first + "Вторая кость: " + second + '<br>';
+    //"Выпал дубль. Число" + i + '<br>' + "Большой разброс между костями. Разница составляет" + difecence + '<br>';
 }
 
-var result = function setResult() { //склеивать все строки в одну
-    writeResult += "Первая кость: " + first + "Вторая кость: " + second + '<br>' +
-        "Выпал дубль. Число" + i + '<br>' + "Большой разброс между костями. Разница составляет" + difecence + '<br>';
-}
-result();
-
-function isNumbersEqual(first, second) { // проверка на совпадение
-    if (first === second || second === first) {
-        elemResult += "Выпал дубль. Число" + i + '<br>';
+function isNumbersEqual(firstPar, secondPar) { // проверка на совпадение
+    if (firstPar === secondPar || secondPar === firstPar) {
+        writeResult += "Выпал дубль. Число" + first + '<br>';
     }
-    return isNumbersEqual() + result();
 }
 
-function isBigDifference(first, second) { // определение разницы
-    if (first < 3 & second > 4 || first > 4 & second < 3) {
-        var difecence = Math.abs(second - first);
-        result();
+function isBigDifference(firstPar, secondPar) { // определение разницы
+    if (firstPar < 3 & secondPar > 4 || firstPar > 4 & secondPar < 3) {
+        var difecence = Math.abs(secondPar - firstPar);
+        result("Большой разброс между костями. Разница составляет" + difecence + '<br>');
     }
-    return isBigDifference;
 }
 
-function isTotal() { 
-    var total = 0;
-    (total < 100) ?
-    result("Победа, вы набрали: " + total +" очков"):
-        result('Вы проиграли, у вас' + total + 'очков');
+function isTotal() {
+    (total > 100) ?
+    result("Победа, вы набрали: " + total + " очков"): result('Вы проиграли, у вас' + total + 'очков');
     //розпечатьть с помощью result
+}
+
+function setTotal(firstPar, secondPar) {
+    total += firstPar + secondPar;
 }
 
 function printResult() {
     elemResult += result;
 }
 
-function run() {
+(function run() {
     for (let i = 1; i <= 15; i++) {
         if (i === 8 || i === 13) {
             continue;
         }
-        var first = getRndNumber();
-        /*let ?*/ var second = getRndNumber();
-        getRndNumber();
-        result();
-        isNumbersEqual();
-        isBigDifference();
-        isTotal();
+        let first = getRndNumber();
+        let second = getRndNumber();
+        result("Первая кость: " + first + "Вторая кость: " + second + '<br>');
+        isNumbersEqual(first, second);
+        isBigDifference(first, second);
+        setTotal();
     }
-}
-run();
+    isTotal();
+    printResult();
+}());
